1526b04d7d97aa314fd4fc5975565592f1d40812
Cyberpew <root@cyberpew.me>
2013-01-29 14:37:22 -0800

    @SuppressWarnings because Eclipse likes to bitch.

--- XirmeCloud/src/me/xir/xirmecloud/TestCommand.java
+++ XirmeCloud/src/me/xir/xirmecloud/TestCommand.java
@@ -5,6 +5,7 @@ import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
 
 class TestCommand extends Command {
+	@SuppressWarnings("unused")
 	private XirmeCloud plugin;
 	
 	public TestCommand(XirmeCloud plugin) {
