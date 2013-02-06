c2f08b4ea94fb625f9d0a830125745c812cade5a
kdude63 <me@kdude63.com>
2013-02-05 22:28:29 -0800

    Fixed /website command.

--- src/me/xir/xirmecloud/commands/commands_website.java
+++ src/me/xir/xirmecloud/commands/commands_website.java
@@ -13,7 +13,7 @@ public class commands_website extends Command {
 	@Override
 	public void execute(CommandSender sender, String[] args) {
 		if (sender.hasPermission("xirmecloud.commands.website")) {
-			sender.sendMessage("Click this link to go to the website -->"+ChatColor.YELLOW+"http://xir.me/"+ChatColor.RESET);
+			sender.sendMessage("Click this link to go to the website --> "+ChatColor.YELLOW+"http://xir.me/"+ChatColor.RESET);
 		} else {
 			sender.sendMessage(ChatColor.RED+"You don't have sufficient permissions to use this command."+ChatColor.RESET);
 		}
