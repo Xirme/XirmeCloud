caa05bb399e4c65528a9c0f38d91a860f185a90f
Cyberpew <root@cyberpew.me>
2013-08-11 15:41:32 -0700

    More updates from the old API formats to the new API formats... let's try this build.

--- src/me/xir/xirmecloud/commands/teleportation/cmd_pvp.java
+++ src/me/xir/xirmecloud/commands/teleportation/cmd_pvp.java
@@ -1,6 +1,6 @@
 package me.xir.xirmecloud.commands.teleportation;
 
-import net.md_5.bungee.BungeeCord;
+import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
@@ -21,7 +21,7 @@ public class cmd_pvp extends Command {
 			
 		    ProxiedPlayer player = (ProxiedPlayer) sender;
 		    
-		    player.connect(BungeeCord.getInstance().config.getServers().get("pvp"));
+		    player.connect(ProxyServer.getInstance().getServerInfo("pvp"));
 		    }
 		}
 	}	
